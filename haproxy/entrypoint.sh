253e6f00d007ada99c2d754477d19cc45c0b9716
Martin Minka <martin.minka@gmail.com>
2015-05-11 11:33:57 +0200

    not needed entrypoint.sh for history

--- haproxy/entrypoint.sh
+++ haproxy/entrypoint.sh
@@ -23,12 +23,22 @@ n=0
 # test: confd -onetime -node "$ETCD_NODE" -keep-stage-file
 echo "[haproxy-confd] waiting for initial haproxy.cfg refresh"
 until confd -onetime -node "$ETCD_NODE"; do
-  if [ "$n" -eq "4" ];  then config_fail; fi
+  if [ "$n" -eq "4" ];  then
+      # config_fail;
+      n=5
+  else
+    n=$((n+1))
+  fi
   echo "[haproxy-confd] haproxy.cfg generatio process failed, waiting again"
-  n=$((n+1))
   sleep $n
 done
 
 echo "[haproxy-confd] Initial HAProxy config created. Starting confd"
 
 confd -node "$ETCD_NODE"
+
+## Run confd in the background to watch the upstream servers
+#confd -interval 10 -node $ETCD -config-file /etc/confd/conf.d/crt-list.toml >> /var/log/output.log 2>&1 &
+#confd -interval 10 -node $ETCD -config-file /etc/confd/conf.d/haproxy.toml >> /var/log/output.log 2>&1 &
+#
+#tail -f /var/log/output.log
