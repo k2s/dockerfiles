cff59ab5cfdbe1c6ec30ad1ec1f7fa43a1b83ea2
Martin Minka <martin.minka@gmail.com>
2015-05-14 17:37:26 +0200

    devtools: add ci.sh to build releases

---
+++ devtools/ci.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+### fail on each failed exit code
+set -e
+
+[ -d /prj/ci ] || mkdir -p /prj/ci
+
+if [ -n "$DOWNLOAD_RSYNC" ]; then
+  # TODO
+  echo "populate /prj/ci folder from rsync"
+fi
+
+if [ -n "$DOWNLOAD_URL" ]; then
+  # TODO
+  echo "populate /prj/ci folder from zip file stored on url"
+fi
+
+if [ ! -f /prj/ci/build.sh ]; then
+  echo "nothing to do because /prj/ci/build.sh was not found"
+  exit 1
+fi
+
+/prj/ci/build.sh
+
+if [ -d /prj/release ]; then
+  if [ -n "$UPLOAD_RSYNC" ]; then
+    echo "uploading release to rsync target"
+    rsync -a --delete /prj/release $UPLOAD_RSYNC
+  fi
+else
+  echo "build failed because /prj/release folder was not found"
+  exit 2
+fi
