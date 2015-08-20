eab135193915f4de651d6712b4f56a12099aa500
Martin Minka <martin.minka@gmail.com>
2015-08-21 00:43:40 +0200

    some not tested stuff

--- build-local.sh
+++ build-local.sh
@@ -1,29 +1,48 @@
 #!/usr/bin/env bash
 
+set -x
+
+# TODO monito solution https://www.brianchristner.io/how-to-setup-docker-monitoring/
+# TODO wordpress
+
 ORDER=(
-    base-deb-minimal
-    base-deb
-    proxy-cache
-    base-jdk7
-    base-jdk8
-    devtools
-    cloudtools
-    youtrack
-    openfire
-    rhc
-    haproxy
+  base-deb-minimal
+  base-deb
+  base-deb-tools
+  proxy-cache
+  mailhub
+  devtools
+  admintools
+  cloudtools
+  haproxy
+#  monitor-nagios
+#  puppetserver
+  base-jdk7
+  base-jdk8
+  youtrack
+  openfire
+  nfs
+#  rhc
+#  rundeck
+  rsync
+#  prj-tmp
+#  runtime
+#  runtime-nodejs
+  shell
+  sphinxsearch
+  php-56-fpm
+  nginx
+  mongodb
 )
 
-#    rundeck
 #    gocd-server
 #    gocd-agent
-#    rundeck
 DEBUG=0
 
 if [ ! -z $1 ]; then
-    DEBUG=0
-    ORDER=()
-    ORDER=$@
+  DEBUG=0
+  ORDER=()
+  ORDER=$@
 fi
 
 export ORDER
@@ -31,21 +50,25 @@ export ORDER
 echo "build all docker images"
 for fld in "${ORDER[@]}"
 do
-    echo "> start build of $fld"
-    if [ $DEBUG ]; then
-        docker build -t bigm/$fld $fld/
-    else
-        docker build -t bigm/$fld $fld/ > /dev/null
-    fi
-    if [ $? -ne 0 ]; then
-    	echo "!!! error in image: $fld"
-    	exit
-    fi
+  echo "> start build of $fld"
+  if [ $DEBUG ]; then
+    # --no-cache
+    docker build -t bigm/$fld $fld/
+  else
+    docker build -t bigm/$fld $fld/ > /dev/null
+  fi
+  if [ $? -ne 0 ]; then
+    echo "!!! error in image: $fld"
+    exit
+  fi
 done
 
-echo "all images build"
+### https://github.com/docker-in-practice/docker-image-graph
+#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock centurylink/image-graph > /tmp/docker_images.png
 
-#docker run --rm -ti -p 80:8080 bigm/youtrack
+echo "all images build, see /tmp/docker_images.png"
+
+#docker run --name nagios -v /tmp/nagios3:/etc/nagios3 --rm -ti -p 80:80 bigm/monitor-nagios
 
 #docker run --rm -ti -p 5222:5222 -p 5223:5223 -p 5224:5224 -p 9090:9090 \
 #    -v /tmp/openfire:/prj/data/openfire \
--- build-release.sh
+++ build-release.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
+set -x
+
+tag=latest
+
 source ./build-local.sh $@
 
 echo "push images to Docker registry"
 
-for fld in "${ORDER[@]}"
-do
-    echo "> push $fld"
-    docker tag -f bigm/$fld:latest quay.io/bigm/$fld:latest
-    docker push quay.io/bigm/$fld:latest
-    if [ $? -ne 0 ]; then
-    	echo "!!! error in image: $fld"
-    	exit
-    fi
+for fld in "${ORDER[@]}"; do
+  echo "> push $fld"
+  docker tag -f bigm/${fld}:latest quay.io/bigm/${fld}:${tag}
+  docker push quay.io/bigm/${fld}:${tag}
+  if [ $? -ne 0 ]; then
+    echo "!!! error in image: $fld"
+    exit
+  fi
 done
 
 echo "all images pushed"
